f0ca45e7990fafe67d27103f93f0f5185cc029b1
David White <dwhite0627@gmail.com>
2020-10-15 21:48:15 -0400

    Add the db model for the location router with full CRUD operations

---
+++ api/location/locationModel.js
@@ -0,0 +1,23 @@
+const db = require('../../data/db-config');
+
+module.export = { findAll, findBy, findByGroomerId, insert, remove };
+
+function findAll() {
+  return db('locations');
+}
+
+function findBy(field) {
+  return db('locations').where(field)
+}
+
+function findByGroomerId(id) {
+  return db('locations').where('groomer_id', id)
+}
+
+function insert(location) {
+  return db('locations').insert(location).returning('*');
+}
+
+function remove(id) {
+  return db('locations').where({ id }).del();
+}
