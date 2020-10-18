cd65174ccf30e94334a9892f68040ddb7108c2ee
David White <dwhite0627@gmail.com>
2020-10-18 19:49:42 -0400

    Add errDetail function to handle and log endpoint errors

---
+++ api/utils/utils.js
@@ -0,0 +1,10 @@
+module.exports = { errDetail };
+
+function errDetail(res, err) {
+  console.log(err);
+  return res.status(500).json({
+    message: 'There was a problem completing the required operation',
+    validation: [],
+    data: {},
+  });
+}
