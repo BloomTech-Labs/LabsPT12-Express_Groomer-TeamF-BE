e35e63c58a497bcee107c5139418154f38060bd5
David White <dwhite0627@gmail.com>
2020-11-04 17:51:39 -0500

    Add assertions to tests with mocks to ensure that the mocks were called

--- __tests__/routes/locations.test.js
+++ __tests__/routes/locations.test.js
@@ -15,6 +15,9 @@ describe('locations router endpoints', () => {
   beforeAll(() => {
     // This is the module/route being tested
     server.use(['/locations'], locationRouter);
+  });
+
+  beforeEach(() => {
     jest.clearAllMocks();
   });
 
@@ -25,6 +28,9 @@ describe('locations router endpoints', () => {
       const res = await request(server).get('/locations');
       expect(res.status).toBe(200);
       expect(res.body.data.length).toBe(0);
+
+      // Verify that the mock was called
+      expect(Locations.findAll.mock.calls.length).toBe(1);
     });
   });
 
@@ -53,6 +59,9 @@ describe('locations router endpoints', () => {
       expect(first.phoneNumber).toBe('414-962-7162');
       expect(first.lat).toBe(-53.91525);
       expect(first.lng).toBe(-96.60353);
+
+      // Verify that the mock was called
+      expect(Locations.findBy.mock.calls.length).toBe(1);
     });
 
     it("should return a 404 code when it can't find the id", async () => {
@@ -61,6 +70,9 @@ describe('locations router endpoints', () => {
 
       expect(res.status).toBe(404);
       expect(res.body.message).toBe('Location Not Found');
+
+      // Verify that the mock was called
+      expect(Locations.findBy.mock.calls.length).toBe(1);
     });
   });
 
@@ -93,11 +105,14 @@ describe('locations router endpoints', () => {
         .put(`/locations/hgf5rfgh54`)
         .send(location);
       const first = res.body.data[0];
-      console.log([res.status, res]);
 
       expect(res.status).toBe(200);
       expect(first.groomerId).not.toBe(`hgf5rfgh54`);
       expect(first.groomerId).toBe(`245feafeffehes`);
+
+      // Verify that the mocks were called
+      expect(Locations.findByGroomerId.mock.calls.length).toBe(1);
+      expect(Locations.update.mock.calls.length).toBe(1);
     });
   });
 });
