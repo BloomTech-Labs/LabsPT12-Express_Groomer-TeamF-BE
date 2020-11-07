965fd8abf13a01b6ccc1c308c8e3da0c9e64fc8c
David White <dwhite0627@gmail.com>
2020-11-07 17:31:33 -0500

    Regroup fake data generators in generate.js

--- test/utils/generate.js
+++ test/utils/generate.js
@@ -1,8 +1,13 @@
 const faker = require('faker');
 
 // Faker generators
-// Profile data
+// Common data
 const getId = () => faker.random.uuid();
+const getCity = () => faker.address.city();
+const getState = () => faker.address.stateAbbr();
+const getZipCode = () => faker.address.zipCode();
+
+// Profile data
 const getOktaId = () => faker.random.alphaNumeric(20);
 const getImg = () => faker.image.avatar();
 const getEmail = () => faker.internet.email();
@@ -14,9 +19,6 @@ const getLocationName = () => faker.company.companyName();
 const getPhoneNumber = () => faker.phone.phoneNumberFormat();
 const getLat = () => faker.address.latitude();
 const getLng = () => faker.address.longitude();
-const getCity = () => faker.address.city();
-const getState = () => faker.address.stateAbbr();
-const getZipCode = () => faker.address.zipCode();
 
 // Test object factory functions
 // User
@@ -26,6 +28,10 @@ function buildUser(overrides = {}) {
     avatarUrl: getImg(),
     email: getEmail(),
     name: getName(),
+    address: getAddress(),
+    city: getCity(),
+    state: getState(),
+    zip: getZipCode(),
     ...overrides,
   };
 }
@@ -37,13 +43,13 @@ function buildLocation(overrides = {}) {
     groomerId: getOktaId(),
     businessName: getLocationName(),
     address: getAddress(),
+    city: getCity(),
+    state: getState(),
+    zip: getZipCode(),
     email: getEmail(),
     phoneNumber: getPhoneNumber(),
     lat: getLat(),
     lng: getLng(),
-    city: getCity(),
-    state: getState(),
-    zip: getZipCode(),
     ...overrides,
   };
 }
