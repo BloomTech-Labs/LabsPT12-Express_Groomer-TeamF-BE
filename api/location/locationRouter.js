7eae6a47f041453555d8b1aaa94f46cf551ca08d
David White <dwhite0627@gmail.com>
2020-11-19 20:34:38 -0500

    Add swagger comments to the locationRouter

--- api/location/locationRouter.js
+++ api/location/locationRouter.js
@@ -23,7 +23,7 @@ const authRequired = require('../middleware/authRequired');
  *        - groomerId
  *      properties:
  *        id:
- *          type: int64
+ *          type: string
  *          description: The record id
  *        groomerId:
  *          type: string
@@ -37,6 +37,15 @@ const authRequired = require('../middleware/authRequired');
  *        address:
  *          type: string
  *          description: The location's address
+ *        city:
+ *          type: string
+ *          description: The location's city
+ *        state:
+ *          type: string
+ *          description: The location's state
+ *        zip:
+ *          type: string
+ *          description: The location's zip code
  *        email:
  *          type: string
  *          description: The business's email address
@@ -59,18 +68,18 @@ const authRequired = require('../middleware/authRequired');
  *      - locations
  *    responses:
  *      200:
- *        description: Success
+ *        description: The success message after fetching all pets
  *        content:
  *          application/json:
  *            schema:
  *              type: object
  *              items:
- *                $ref: '#/components/schemas/Locations'
+ *                $ref: '#/components/schemas/Pets'
  *              properties:
  *                message:
  *                  type: string
  *                  description: A status message
- *                  example: 'Success'
+ *                  example: Success
  *                validation:
  *                  type: array
  *                  description: An array of validation errors
@@ -78,15 +87,29 @@ const authRequired = require('../middleware/authRequired');
  *                data:
  *                  type: array
  *                  description: The data returned from the endpoint
- *                example:
- *                  - id: 2
- *                    groomerId: '00ultwz1n9ORpNFc04x6'
- *                    businessName: "Gillian's Fine Pet Grooming"
- *                    address: faker.address.streetAddress('###')
- *                    email: faker.internet.email()
- *                    phoneNumber: faker.phone.phoneNumberFormat()
- *                    lat: -77.0497
- *                    lng: 38.9007
+ *                  example:
+ *                    - id: 1
+ *                      groomerId: 00wi8Swz1n9ORpNFc04x6
+ *                      businessName: Madame's Pet Styles
+ *                      address: 25 Winding Lane
+ *                      city: Los Angeles
+ *                      state: CA
+ *                      zip: 90210
+ *                      email: madame@petstyles.io
+ *                      phoneNumber: 903-101-2203
+ *                      lat: -77.012
+ *                      lng: 38.897
+ *                    - id: 2
+ *                      groomerId: 00ultwz1n9ORpNFc04x6
+ *                      businessName: Gillians Fine Pet Grooming
+ *                      address: 100 Main Street
+ *                      city: Los Angeles
+ *                      state: CA
+ *                      zip: 90210
+ *                      email: gillian@finepetgrooming.com
+ *                      phoneNumber: 902-111-2222
+ *                      lat: -77.0497
+ *                      lng: 38.9007
  *      404:
  *        $ref: '#/components/responses/LocationNotFound'
  *      500:
@@ -100,6 +123,87 @@ router.get('/', authRequired, async (req, res) => {
       validation: [],
       data: locationData,
     });
+  } catch (err) {
+    errDetail(res, err);
+  }
+});
+
+/**
+ * @swagger
+ *  components:
+ *   parameters:
+ *     groomerId:
+ *      in: path
+ *      description: The groomerId, which is the sub from the okta auth claims
+ *      required: true
+ *      example: 00ultwz1n9ORpNFc04x6
+ *      schema:
+ *        type: string
+ * /locations/:groomerId:
+ *  get:
+ *    description: Returns an array of location objects for this user
+ *    summary: Get a location owned by a user
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - locations
+ *    responses:
+ *      200:
+ *        description: The success message after fetching a pet
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              items:
+ *                $ref: '#/components/schemas/Pets'
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: A status message
+ *                  example: Success
+ *                validation:
+ *                  type: array
+ *                  description: An array of validation errors
+ *                  example: []
+ *                data:
+ *                  type: array
+ *                  description: The data returned from the endpoint
+ *                  example:
+ *                    - id: 1
+ *                      groomerId: 00wi8Swz1n9ORpNFc04x6
+ *                      businessName: Madame's Pet Styles
+ *                      address: 25 Winding Lane
+ *                      city: Los Angeles
+ *                      state: CA
+ *                      zip: 90210
+ *                      email: madame@petstyles.io
+ *                      phoneNumber: 903-101-2203
+ *                      lat: -77.012
+ *                      lng: 38.897
+ *      404:
+ *        $ref: '#/components/responses/LocationNotFound'
+ *      500:
+ *        $ref: '#/components/responses/ServerError'
+ */
+router.get('/:groomerId', authRequired, async (req, res) => {
+  try {
+    const groomerId = String(req.params.groomerId);
+    const location = await findBy({ groomerId });
+
+    if (location && location.length) {
+      const locationRes = {
+        message: 'Success',
+        validation: [],
+        data: location,
+      };
+      res.status(200).json(locationRes);
+    } else {
+      res.status(404).json({
+        message: 'Location Not Found',
+        validation: [],
+        data: [],
+      });
+    }
   } catch (err) {
     errDetail(res, err);
   }
@@ -149,9 +253,9 @@ router.get('/', authRequired, async (req, res) => {
  *        lon:
  *          type: float
  *          description: The longitude coordinate for the business address
- * /locations:
- *  get:
- *    description: Returns an array of location objects for this user
+ * /locations/:groomerId:
+ *  delete:
+ *    description: Returns an object indicating success or failure of deletion
  *    summary: Get a list of locations owned by a user
  *    security:
  *      - okta: []
@@ -170,7 +274,70 @@ router.get('/', authRequired, async (req, res) => {
  *                message:
  *                  type: string
  *                  description: A status message
- *                  example: 'Success'
+ *                  example: The location for groomer 00ultwz1n9ORpNFc04x6 was successfully deleted.
+ *                validation:
+ *                  type: array
+ *                  description: An array of validation errors
+ *                  example: []
+ *                data:
+ *                  type: integer
+ *                  description: The data returned from the endpoint
+ *                example: 1
+ *      500:
+ *        $ref: '#/components/responses/ServerError'
+ */
+router.delete('/:groomerId', authRequired, async (req, res) => {
+  const id = String(req.params.groomerId);
+  try {
+    db.remove(id).then((location) => {
+      res.status(200).json({
+        message: `The location for groomer '${id}' was successfully deleted.`,
+        validation: [],
+        data: location,
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Unable to delete',
+      validation: [],
+      data: {},
+    });
+  }
+});
+
+/**
+ * @swagger
+ *  components:
+ *   parameters:
+ *     groomerId:
+ *      in: path
+ *      description: The groomerId, which is the sub from the okta auth claims
+ *      required: true
+ *      example: 00ultwz1n9ORpNFc04x6
+ *      schema:
+ *        type: string
+ * /locations/:groomerId:
+ *  put:
+ *    description: Returns an array of location objects for this user
+ *    summary: Get a location owned by a user
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - locations
+ *    responses:
+ *      200:
+ *        description: The success message after updating a pet
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              items:
+ *                $ref: '#/components/schemas/Pets'
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: A status message
+ *                  example: Successfully updated location 00ultwqjtqt4VCcS24x6
  *                validation:
  *                  type: array
  *                  description: An array of validation errors
@@ -178,60 +345,23 @@ router.get('/', authRequired, async (req, res) => {
  *                data:
  *                  type: array
  *                  description: The data returned from the endpoint
- *                example:
- *                  - id: 2
- *                    groomerId: '00ultwz1n9ORpNFc04x6'
- *                    businessName: "Gillian's Fine Pet Grooming"
- *                    address: faker.address.streetAddress('###')
- *                    email: faker.internet.email()
- *                    phoneNumber: faker.phone.phoneNumberFormat()
- *                    lat: -77.0497
- *                    lng: 38.9007
+ *                  example:
+ *                    - id: 1
+ *                      groomerId: 00wi8Swz1n9ORpNFc04x6
+ *                      businessName: Madame's Pet Styles
+ *                      address: 25 Winding Lane
+ *                      city: Los Angeles
+ *                      state: CA
+ *                      zip: 90210
+ *                      email: madame@petstyles.io
+ *                      phoneNumber: 903-101-2203
+ *                      lat: -77.012
+ *                      lng: 38.897
  *      404:
  *        $ref: '#/components/responses/LocationNotFound'
  *      500:
  *        $ref: '#/components/responses/ServerError'
  */
-router.get('/:groomerId', authRequired, async (req, res) => {
-  try {
-    const groomerId = String(req.params.groomerId);
-    const location = await findBy({ groomerId });
-
-    if (location && location.length) {
-      const locationRes = {
-        message: 'Success',
-        validation: [],
-        data: location,
-      };
-      res.status(200).json(locationRes);
-    } else {
-      res.status(404).json({
-        message: 'Location Not Found',
-        validation: [],
-        data: [],
-      });
-    }
-  } catch (err) {
-    errDetail(res, err);
-  }
-});
-
-router.delete('/:groomerId', authRequired, async (req, res) => {
-  const id = String(req.params.groomerId);
-  try {
-    db.remove(id).then((location) => {
-      res.status(200).json({
-        message: `Profile '${id}' was successfully deleted.`,
-        data: location,
-      });
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: 'Unable to delete',
-    });
-  }
-});
-
 router.put('/:groomerId', authRequired, function (req, res) {
   const location = req.body;
   if (location) {
@@ -242,7 +372,7 @@ router.put('/:groomerId', authRequired, function (req, res) {
           .update(id, location)
           .then((updated) => {
             res.status(200).json({
-              message: `Successfully updated profile ${id}`,
+              message: `Successfully updated location ${id}`,
               data: updated,
             });
           })
@@ -262,6 +392,63 @@ router.put('/:groomerId', authRequired, function (req, res) {
   }
 });
 
+/**
+ * @swagger
+ *  components:
+ *   parameters:
+ *     groomerId:
+ *      in: path
+ *      description: The groomerId, which is the sub from the okta auth claims
+ *      required: true
+ *      example: 00ultwz1n9ORpNFc04x6
+ *      schema:
+ *        type: string
+ * /locations/:groomerId:
+ *  post:
+ *    description: Returns an array of location objects for this user
+ *    summary: Get a location owned by a user
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - locations
+ *    responses:
+ *      200:
+ *        description: The success message after adding a pet
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              items:
+ *                $ref: '#/components/schemas/Pets'
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: A status message
+ *                  example: Location created
+ *                validation:
+ *                  type: array
+ *                  description: An array of validation errors
+ *                  example: []
+ *                data:
+ *                  type: array
+ *                  description: The data returned from the endpoint
+ *                  example:
+ *                    - id: 1
+ *                      groomerId: 00wi8Swz1n9ORpNFc04x6
+ *                      businessName: Madame's Pet Styles
+ *                      address: 25 Winding Lane
+ *                      city: Los Angeles
+ *                      state: CA
+ *                      zip: 90210
+ *                      email: madame@petstyles.io
+ *                      phoneNumber: 903-101-2203
+ *                      lat: -77.012
+ *                      lng: 38.897
+ *      404:
+ *        $ref: '#/components/responses/LocationNotFound'
+ *      500:
+ *        $ref: '#/components/responses/ServerError'
+ */
 router.post('/', authRequired, async (req, res) => {
   let location = req.body;
   const id = location.groomerId;
@@ -271,7 +458,7 @@ router.post('/', authRequired, async (req, res) => {
         if (undef == undefined) {
           await db.insert(location).then((location) =>
             res.status(200).json({
-              message: 'location created',
+              message: 'Location created',
               data: location,
             })
           );
